acadbebc7e9461963110c33a08d22f9434ea5ca2
redaei <redaeis@gmail.com>
2025-02-11 19:23:45 +0300

    applied the layout
    and the theme for sign in page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import './css/style.css'
+import './css/satoshi.css'
+import 'jsvectormap/dist/jsvectormap.css'
+import 'flatpickr/dist/flatpickr.min.css'
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <App />
-</BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,51 +1,162 @@
-import { useState } from "react"
-import axios from "axios"
-import { useNavigate, Link } from "react-router-dom"
-import Swal from "sweetalert2"
-
-const SignIn = (props)=>{
-
-    const navigate = useNavigate()
-
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
-    async function handleLogin(e){
-        e.preventDefault()
-        try {
-            const requestBody = {email, password}
-            const response = await axios.post('https://api.p2.lc2s5.foxhub.space/login', requestBody)
-            localStorage.setItem('access_token', response.data.access_token)
-            navigate('/')
-        } catch (error) {
-            console.log(error);
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: error.response.data.message
-            });
-        }
+import { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import * as authService from './../services/authService'
+import Swal from 'sweetalert2'
+const initialFormData = {
+  username: '',
+  password: ''
+}
+const SignIn = (props) => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState(initialFormData)
+  const [message, setMessage] = useState('')
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await authService.signin(formData)
+      console.log(user)
+      if (user.error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: user.error
+        })
+      } else {
+        props.setUser(user)
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(`error in singIn handleSubmit: ${error}`)
+      console.log(error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: error.response.data.message
+      })
     }
+  }
+  useEffect(() => {
+    if (authService.getUser()) {
+      navigate('/')
+    }
+  }, [])
 
-    return (
-        <div className="container" style={{marginTop:"10vh"}}>
-            <form onSubmit={handleLogin}>
-                <h2>Login to your account</h2>
-                <p>Welcome back!</p>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address :</label>
-                    <input onChange={e => {setEmail(e.target.value)}} type="email" className="form-control" id="email"/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password :</label>
-                    <input onChange={e => {setPassword(e.target.value)}} type="password" className="form-control" id="password"/>
-                </div>
-                <button type="submit" className="btn btn-primary">LOG IN</button>
-                <p style={{marginTop:"2vh"}}>Don't have an account?<Link to={'/SignUp'}>Create an account</Link></p>
-                <p><br />Demo User: <br />Email: user@example.com <br />Password: password12345</p>
-            </form>
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen h-80">
+        <div className="w-full max-w-md rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+          <div className="flex flex-wrap items-center">
+            <div className="w-full border-stroke dark:border-strokedark xl:border-l-2">
+              <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
+                <span className="mb-1.5 block font-medium">Start for free</span>
+                <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
+                  Sign In to In-Vite
+                </h2>
+
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-4">
+                    <label className="mb-2.5 block font-medium text-black dark:text-white">
+                      Username
+                    </label>
+                    <div className="relative">
+                      <input
+                        onChange={handleChange}
+                        value={formData.username}
+                        type="text"
+                        name="username"
+                        placeholder="Enter your username"
+                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      />
+
+                      <span className="absolute right-4 top-4">
+                        {/* <svg
+                          className="fill-current"
+                          width="22"
+                          height="22"
+                          viewBox="0 0 22 22"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g opacity="0.5">
+                            <path
+                              d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z"
+                              stroke="#000000"
+                              stroke-width="1.224"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              fill="none"
+                            ></path>
+                          </g>
+                        </svg> */}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="mb-2.5 block font-medium text-black dark:text-white">
+                      Password
+                    </label>
+                    <div className="relative">
+                      <input
+                        onChange={handleChange}
+                        value={formData.password}
+                        name="password"
+                        type="password"
+                        placeholder="6+ Characters, 1 Capital letter"
+                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      />
+
+                      <span className="absolute right-4 top-4">
+                        <svg
+                          className="fill-current"
+                          width="22"
+                          height="22"
+                          viewBox="0 0 22 22"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g opacity="0.5">
+                            <path
+                              d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
+                              fill=""
+                            />
+                            <path
+                              d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
+                              fill=""
+                            />
+                          </g>
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="mb-5">
+                    <input
+                      type="submit"
+                      value="Sign In"
+                      className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90"
+                    />
+                  </div>
+
+                  <div className="mt-6 text-center">
+                    <p>
+                      Don’t have any account?{' '}
+                      <Link to="/SignUp" className="text-primary">
+                        Sign Up
+                      </Link>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </>
+  )
 }
 
 export default SignIn
